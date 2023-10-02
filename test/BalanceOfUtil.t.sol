e423bbc04572f39f992c256ceff4b43e5935321c
malcolmtan <zherui_tan@live.com.sg>
2023-10-02 11:49:05 +0800

    Setup

--- test/BalanceOfUtil.t.sol
+++ test/BalanceOfUtil.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {BalanceOfUtil} from "../src/BalanceOfUtil.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    BalanceOfUtil public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new BalanceOfUtil();
         counter.setNumber(0);
     }
 
