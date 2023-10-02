6bc2d77cbe233b7fbad177b9d5508e8c19f9beb4
malcolmtan <zherui_tan@live.com.sg>
2023-10-02 16:38:30 +0800

    Update contract impl and remappings for Foundry

--- test/BalanceOfUtil.t.sol
+++ test/BalanceOfUtil.t.sol
@@ -4,21 +4,21 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {BalanceOfUtil} from "../src/BalanceOfUtil.sol";
 
-contract CounterTest is Test {
-    BalanceOfUtil public counter;
+contract BalanceOfUtilTest is Test {
+    // BalanceOfUtil public counter;
 
-    function setUp() public {
-        counter = new BalanceOfUtil();
-        counter.setNumber(0);
-    }
+    // function setUp() public {
+    //     counter = new BalanceOfUtil();
+    //     counter.setNumber(0);
+    // }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    // function test_Increment() public {
+    //     counter.increment();
+    //     assertEq(counter.number(), 1);
+    // }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+    // function testFuzz_SetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
 }
